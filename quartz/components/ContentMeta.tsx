bef07fb3ce2baa1ea21f8f62a5fe1f4d38331108
Ronan Morse <ronanm@Ronans-MacBook-Pro.local>
2024-11-18 23:13:42 -0500

    Quartz sync: Nov 18, 2024, 11:13 PM

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -23,37 +23,6 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
   // Merge options with defaults
   const options: ContentMetaOptions = { ...defaultOptions, ...opts }
 
-  function ContentMetadata({ cfg, fileData, displayClass }: QuartzComponentProps) {
-    const text = fileData.text
-
-    if (text) {
-      const segments: (string | JSX.Element)[] = []
-
-      if (fileData.dates) {
-        segments.push(formatDate(getDate(cfg, fileData)!, cfg.locale))
-      }
-
-      // Display reading time if enabled
-      if (options.showReadingTime) {
-        const { minutes, words: _words } = readingTime(text)
-        const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
-          minutes: Math.ceil(minutes),
-        })
-        segments.push(displayedTime)
-      }
-
-      const segmentsElements = segments.map((segment) => <span>{segment}</span>)
-
-      return (
-        <p show-comma={options.showComma} class={classNames(displayClass, "content-meta")}>
-          {segmentsElements}
-        </p>
-      )
-    } else {
-      return null
-    }
-  }
-
   ContentMetadata.css = style
 
   return ContentMetadata
